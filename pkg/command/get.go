c4e7a8cbba65262728165121eb2d000ee6c79bbe
laik <laik.lj@me.com>
2020-10-22 11:22:32 +0800

    add format output

--- pkg/command/get.go
+++ pkg/command/get.go
@@ -3,6 +3,7 @@ package command
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/yametech/echoer/pkg/core"
 
 	"github.com/yametech/echoer/pkg/common"
 	"github.com/yametech/echoer/pkg/storage"
@@ -28,11 +29,29 @@ func (g *Get) Execute(args ...string) Reply {
 	if err := g.Get(common.DefaultNamespace, resType, args[1], &result); err != nil {
 		return &ErrorReply{Message: fmt.Sprintf("resource (%s) (%s) not exist or get error (%s)", resType, args[1], err)}
 	}
+	format := NewFormat()
+	format.Header("name", "type", "version", "data")
 	bs, err := json.Marshal(result)
 	if err != nil {
-		return &ErrorReply{Message: fmt.Sprintf("resource (%s) (%s) unmarshal byte error (%s)", resType, args[1], err)}
+		return &ErrorReply{Message: fmt.Sprintf("get resource (%s) unmarshal byte error (%s)", resType, err)}
 	}
-	return &RawReply{Message: bs}
+	typePointer := storage.GetResourceCoder(resType)
+	obj := typePointer.(core.IObject).Clone()
+	if err := core.JSONRawToResource(bs, obj); err != nil {
+		return &ErrorReply{Message: fmt.Sprintf("get resource (%s) unmarshal byte error (%s)", resType, err)}
+	}
+	var outData string
+	length := len(bs)
+	if length <= 100 {
+		outData = string(bs)
+	} else {
+		bs = bs[length-101:]
+		bs = append([]byte("..."), bs...)
+		outData = string(bs)
+	}
+	format.Row(obj.GetName(), resType, obj.GetUUID(), outData)
+
+	return &RawReply{Message: format.Out()}
 }
 
 func (g *Get) Help() string {
